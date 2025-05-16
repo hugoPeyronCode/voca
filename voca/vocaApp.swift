6f56f7dd3bce9b4a6914109560d649da80504609
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-05-16 13:11:33 +0200

    Initial Commit

---
+++ voca/vocaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vocaApp.swift
+//  voca
+//
+//  Created by Hugo Peyron on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct vocaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
